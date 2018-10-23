3e47a37aa002bc89fae3d3c2eaa069d3ced75632
unknown <you@example.com>
2018-10-23 20:59:31 +0200

    Update token_reader with youtube API

--- token_reader.py
+++ token_reader.py
@@ -5,3 +5,11 @@ def get_token():
         return ret[:len(ret)-2]
     else:
         return ret
+
+def get_yt_api_key():
+    ret = open('yt_api.tf').readline()
+
+    if '\n' in ret:
+        return ret[:len(ret)-2]
+    else:
+        return ret
